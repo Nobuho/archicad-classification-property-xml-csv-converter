2e0e49ff74131b2608dc4c690aa05ec1384e468c
Nobuho <novufo13@gmail.com>
2019-11-23 01:51:43 +0900

    edit

--- archicad_classification_xml_parser.py
+++ archicad_classification_xml_parser.py
@@ -2,20 +2,41 @@ from lxml import etree
 import csv
 from glob import glob
 import codecs
-import re, collections
+import re
 from collections import Counter
 
 
 tree = etree.parse("archicad_classification_xml/archicad_classification_xml.xml")
 root = tree.getroot().tag
-base_path = tree.xpath("/BuildingInformation/Classification/System/Items/Item/Children/Item/ID/chomechome")
+base_path = tree.xpath("/BuildingInformation/Classification/System/Items/Item")
 
-if len(p) == 0:
-    print("none!")
+# if len(p) == 0:
+    # print("none!")
+
+search_xpath = r"/BuildingInformation/Classification/System/Items/Item.*"
+search_tag = r".*ID"
+
+for tag in tree.iter():
+    path = tree.getpath(tag)
+    if re.match(search_xpath, path) and re.match(search_tag, path):
+        level = path.count("Children")
+        print(","*level,tree.xpath(path)[0].text)
 
 
-for elem in base_path:
-    tree.getpath(elem)        
+
+# for elem in base_path:
+#     path = tree.getpath(elem)
+#     elem_name = tree.xpath(path + "/ID")[0].text
+#     print(elem_name)
+#     while True:
+#         child_path = path + "/Children/Item"
+#         child_elem = tree.xpath(child_path + "/ID")
+#         if len(child_elem) == 0:
+#             break
+#         else:
+#             child_elem_name = tree.xpath(child_path + "/ID")[0].text
+#             print(elem_name)
+#             path = path + "/Children/Item"
 
 
 
