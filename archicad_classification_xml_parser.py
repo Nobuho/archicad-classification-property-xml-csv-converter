4451fd22551a425dacc6562c1b7e22d67a0d9a69
Nobuho <novufo13@gmail.com>
2019-11-23 03:16:14 +0900

    done

--- archicad_classification_xml_parser.py
+++ archicad_classification_xml_parser.py
@@ -1,45 +1,91 @@
 from lxml import etree
 import csv
-from glob import glob
 import codecs
 import re
-from collections import Counter
 
 
 tree = etree.parse("archicad_classification_xml/archicad_classification_xml.xml")
-root = tree.getroot().tag
-base_path = tree.xpath("/BuildingInformation/Classification/System/Items/Item")
 
-# if len(p) == 0:
-    # print("none!")
+# ##################################################
+# making classification list
+# ##################################################
 
-search_xpath = r"/BuildingInformation/Classification/System/Items/Item.*"
-search_tag = r".*ID"
+classification_list = []
+
+search_xpath1 = r"/BuildingInformation/Classification/System/Items/Item\[\d\]/ID"
+search_xpath2 = r"/BuildingInformation/Classification/System/Items/Item\[\d\]/Children/Item\[\d\]/ID"
+search_xpath3 = r"/BuildingInformation/Classification/System/Items/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/ID"
+search_xpath4 = r"/BuildingInformation/Classification/System/Items/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/ID"
+search_xpath5 = r"/BuildingInformation/Classification/System/Items/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/ID"
 
 for tag in tree.iter():
+
     path = tree.getpath(tag)
-    if re.match(search_xpath, path) and re.match(search_tag, path):
-        level = path.count("Children")
-        print(","*level,tree.xpath(path)[0].text)
+    text = tree.xpath(path)[0].text
+
+    if re.match(search_xpath1, path):
+        cls1 = text
+        classification_list.append([cls1, cls1, cls1, cls1, cls1])
+
+    elif re.match(search_xpath2, path):
+        cls2 = text
+        classification_list.append([cls1, cls2, cls2, cls2, cls2])
+
+    elif re.match(search_xpath3, path):
+        cls3 = text
+        classification_list.append([cls1, cls2, cls3, cls3, cls3])
+
+    elif re.match(search_xpath4, path):
+        cls4 = text
+        classification_list.append([cls1, cls2, cls3, cls4, cls4])
+
+    elif re.match(search_xpath5, path):
+        cls5 = test
+        classification_list.append([cls1, cls2, cls3, cls4, cls5])
+        
+with codecs.open('classification.csv', 'w', 'cp932', 'ignore') as f:
+    writer = csv.writer(f, delimiter='\t')
+    writer.writerows(classification_list)
+
+# ##################################################
+# making classification property mapping list
+# ##################################################
+
+mapping_list = []
+
+search_xpath_property = r"/BuildingInformation/PropertyDefinitionGroups/PropertyDefinitionGroup\[\d*\]/PropertyDefinitions/PropertyDefinition\[\d*\]/Name"
+search_xpath_classification = r"/BuildingInformation/PropertyDefinitionGroups/PropertyDefinitionGroup.*ItemID"
+
+for tag in tree.iter():
+
+    path = tree.getpath(tag)
+    text = tree.xpath(path)[0].text
+
+    if re.match(search_xpath_property, path):
+        classification = text
+
+    elif re.match(search_xpath_classification, path):
+        mapping_list.append([classification, text])
+        
+with codecs.open('classification_property_mapping.csv', 'w', 'cp932', 'ignore') as f:
+    writer = csv.writer(f, delimiter='\t')
+    writer.writerows(mapping_list)
 
 
+# /BuildingInformation/Classification/System/Items/Item\[\d\]/ID
+# /BuildingInformation/Classification/System/Items/Item\[\d\]/Children/Item\[\d\]/ID
+# /BuildingInformation/Classification/System/Items/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/ID
+# /BuildingInformation/Classification/System/Items/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/ID
+# /BuildingInformation/Classification/System/Items/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/Children/Item\[\d\]/ID
 
-# for elem in base_path:
-#     path = tree.getpath(elem)
-#     elem_name = tree.xpath(path + "/ID")[0].text
-#     print(elem_name)
-#     while True:
-#         child_path = path + "/Children/Item"
-#         child_elem = tree.xpath(child_path + "/ID")
-#         if len(child_elem) == 0:
-#             break
-#         else:
-#             child_elem_name = tree.xpath(child_path + "/ID")[0].text
-#             print(elem_name)
-#             path = path + "/Children/Item"
+# /BuildingInformation/PropertyDefinitionGroups/PropertyDefinitionGroup[7]/PropertyDefinitions/PropertyDefinition[4]/ClassificationIDs/ClassificationID/ItemID
+# /BuildingInformation/PropertyDefinitionGroups/PropertyDefinitionGroup[7]/PropertyDefinitions/PropertyDefinition[2]/Name
 
+# search_xpath = r"/BuildingInformation/Classification/System/Items/Item.*"
+# search_tag = r".*ID"
 
-
-# /BuildingInformation/Classification/System/Items/Item[1]/ID
-# /BuildingInformation/Classification/System/Items/Item[1]/Children/Item[1]/ID
-# /BuildingInformation/Classification/System/Items/Item[1]/Children/Item[3]/Children/Item[1]/ID
+# for tag in tree.iter():
+#     path = tree.getpath(tag)
+#     if re.match(search_xpath, path) and re.match(search_tag, path):
+#         level = path.count("Children")
+#         print(","*level,tree.xpath(path)[0].text)
