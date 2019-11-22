417cb6e46cf9e8f09506e6cb4ec679e4318f3837
Nobuho <novufo13@gmail.com>
2019-11-22 23:23:27 +0900

    cs

--- archicad_classification_xml_parser.py
+++ archicad_classification_xml_parser.py
@@ -3,9 +3,13 @@ import csv
 from glob import glob
 import codecs
 
+
 import re, collections
+from lxml import etree
   
-xml = '''\
+for f in glob("archicad_classification_xml/*.xml"):
+
+xml = f
 <data>
     <timestamp>not important</timestamp>
     <people>
@@ -47,18 +51,18 @@ xml = '''\
     </cities>
 </data>
 '''
-
+ 
 xml_root = etree.fromstring(xml)
 raw_tree = etree.ElementTree(xml_root)
 nice_tree = collections.OrderedDict()
-
+ 
 for tag in xml_root.iter():
     path = re.sub('\[[0-9]+\]', '', raw_tree.getpath(tag))
     if path not in nice_tree:
         nice_tree[path] = []
     if len(tag.keys()) > 0:
-        nice_tree[path].extend(attrib for attrib in tag.keys() if attrib not in nice_tree[path])
-        
+        nice_tree[path].extend(attrib for attrib in tag.keys() if attrib not in nice_tree[path])            
+ 
 for path, attribs in nice_tree.items():
     indent = int(path.count('/') - 1)
     print('{0}{1}: {2} [{3}]'.format('    ' * indent, indent, path.split('/')[-1], ', '.join(attribs) if len(attribs) > 0 else '-'))
